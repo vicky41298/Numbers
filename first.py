9bab44b70c9908d68f65d472752849314cd6f831
vicky41298 <vicky41298@gmail.com>
2019-07-31 13:31:20 +0530

    Create first.py

---
+++ first.py
@@ -0,0 +1,22 @@
+def insert(index, level):
+    for i in range(n):
+        if parents[i]==index:
+            if (len(nodes)<level+1):
+                nodes.append([])
+            nodes[level].append(i)
+            insert(i, level+1)
+n=int(input())
+parents=[]
+nodes=[]
+lst=[]
+parents=list(map(int,input().split()))
+for i in range (n) :
+    if parents[i]==-1:
+        if(len(nodes)<1) :
+            nodes.append([])
+        nodes[0].append(i)
+        insert(i,1)
+for i in nodes:
+    #print(*i,sep=' ')
+    lst.append(len(i))
+print(max(lst))
