ddbe5920716432d11b0ad9dfacfe9062ecce44c5
vicky41298 <vicky41298@gmail.com>
2019-07-31 12:54:16 +0530

    Create Ancestor.py

---
+++ Ancestor.py
@@ -0,0 +1,30 @@
+def traverseback(parent,x):
+if(parent==-1):
+x.append(-1)
+return x
+
+X.append(parent)
+return traverseback (1[parent],x)
+1=List(map(int, input().split()))
+a,b=map(int,input().split())
+parents, parents B=[a],[b]
+parents, parents=traverseback(fa) parentsA), traverseback (1[6], parents)
+print(1, parents, parents)
+for i in range(1, max(len(parents), on(parents))):
+if(parents[in] =parents Bi]):
+
+
+print(parents[-i+1])
+exit()
+
+
+
+if(len (parents) <len(parents)):
+prank(parents[@])
+
+
+
+else:
+
+
+print(parents Be])
