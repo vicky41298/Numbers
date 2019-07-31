fd93707455086a06ccaf9861d523cd88aa47ec8b
vicky41298 <vicky41298@gmail.com>
2019-07-31 13:43:39 +0530

    Update Ancestor.py

--- Ancestor.py
+++ Ancestor.py
@@ -1,30 +1,19 @@
 def traverseback(parent,x):
     if(parent==-1):
-       x.append(-1)
-       return x
-
+        x.append(-1)
+        return x
     x.append(parent)
-    return traverseback (l[parent],x)
-l=List(map(int, input().split()))
+    return traverseback(l[parent],x)
+l=list(map(int,input().split()))
 a,b=map(int,input().split())
 parentsA, parentsB=[a],[b]
-parentsA, parentsB=traverseback(l[a], parentsA), traverseback (1[b], parentsB)
-print(l, parentsA, parentsB)
-for i in range(1, max(len(parentsA), len(parentsB))):
+parentsA, parentsB=traverseback(l[a], parentsA), traverseback(l[b], parentsB)
+#print(l, parentsA, parentsB)
+for i in range(1,max(len(parentsA),len(parentsB))):
     if(parentsA[-i]!=parentsB[-i]):
-
-
         print(parentsA[-i+1])
         exit()
-
-
-
-if(len (parentsA) <len(parentsB)):
-        print(parentsA[0])
-
-
-
+if(len (parentsA)<len(parentsB)):
+    print(parentsA[0])
 else:
-
-
-      print(parentsB[0])
+    print(parentsB[0])
