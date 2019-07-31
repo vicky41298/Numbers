497ed87d93ee1925b6a557583aa0d2794f1763b1
vicky41298 <vicky41298@gmail.com>
2019-07-31 13:45:37 +0530

    Update levelorder.py

--- levelorder.py
+++ levelorder.py
@@ -6,17 +6,14 @@ def insert(index, level):
             nodes[level].append(i)
             insert(i, level+1)
 n=int(input())
-parents = []
+parents=[]
 nodes=[]
-parents= list(map(int,input().split()))
+parents=list(map(int,input().split()))
 for i in range (n) :
     if parents[i]==-1:
-       if(len(nodes)<1) :
-
+        if(len(nodes)<1) :
             nodes.append([])
-
-       nodes[0].append(i)
-       insert(i,1)
+        nodes[0].append(i)
+        insert(i,1)
 for i in nodes:
     print(*i,sep=' ') 
-#print(max(len(i))) 
